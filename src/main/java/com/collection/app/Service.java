06031fde0243be1d264e04d57dfbf753150d291a
Md. Saidur Rahman <37741326+saidurNsu99@users.noreply.github.com>
2024-05-27 11:18:04 +0600

    arraylist

--- src/main/java/com/collection/app/Service.java
+++ src/main/java/com/collection/app/Service.java
@@ -14,12 +14,21 @@ public class Service {
         s.add(3.14);
         s.add(true);
 
+        // Insert an element at a specific index
+        s.add(4, "Mango");  // Insert "Mango" at index 1
+
+        // Get the element at a specific index
+        String secondElement = String.valueOf(s.get(1));
+
         HashSet<Object> uniqueItems = new HashSet<>();
         for (Object temp : s) {
             if (uniqueItems.add(temp)) {
                 System.out.println("Value for this index is: " + temp);
+
             }
         }
+        System.out.println("second element in the list which is the Value for  index 1 is: " + secondElement);
+
 
         return s;
     }
